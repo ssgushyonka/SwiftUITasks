cc2eb4b0ffbfddc1bf86e5a5559c308d53e62ecc
ssgushyonka <orangeee04@gmail.com>
2025-07-12 23:23:23 +0500

    add world scramble

---
+++ WorldScramble/WorldScramble/WorldScramble/WorldScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldScrambleApp.swift
+//  WorldScramble
+//
+//  Created by Элина Борисова on 12.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct WorldScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
