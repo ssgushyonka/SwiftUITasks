ba9ecd2dc2d8601e82e12bcdbb4a5808ed1c6324
ssgushyonka <orangeee04@gmail.com>
2025-07-12 11:34:34 +0500

    Initial Commit

---
+++ SwiftUITasks/SwiftUITasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITasksApp.swift
+//  SwiftUITasks
+//
+//  Created by Элина Борисова on 12.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
